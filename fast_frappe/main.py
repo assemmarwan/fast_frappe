e9025a69864a0a8de1e1af7d1e61eb26a3e0135d
Assem Marwan <assem905@gmail.com>
2022-01-30 11:29:26 +0400

    feat: Add GraphQL endpoint

--- fast_frappe/main.py
+++ fast_frappe/main.py
@@ -1,8 +1,13 @@
-import frappe
-from typing import Optional
-
-from fastapi import FastAPI
+import graphql
 from fast_frappe.ctrl import init_frappe
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from graphql import GraphQLError
+
+import frappe
+from frappe_graphql import get_schema
+from frappe_graphql.graphql import execute
+from frappe_graphql.utils.resolver import default_field_resolver
 
 app = FastAPI()
 
@@ -16,3 +21,93 @@ def read_root():
         "available_doctypes": available_doctypes,
         "settings": settings.as_dict(),
     }
+
+
+@app.on_event("startup")
+def on_start():
+    init_frappe()
+
+
+@app.post("/graphql")
+async def graphql_resolver(body: dict):
+    graphql_request = frappe.parse_json(body)
+    query = graphql_request.query
+    variables = graphql_request.variables
+    operation_name = graphql_request.operationName
+    # frappe.set_user("administrator")
+    output = await execute_async(query, variables, operation_name)
+
+    if len(output.get("errors", [])):
+        frappe.db.rollback()
+        errors = []
+        for err in output.errors:
+            if isinstance(err, GraphQLError):
+                err = err.formatted
+            errors.append(err)
+        output.errors = errors
+    return output
+
+
+@app.post("/graphql_sync")
+def graphql_resolver_sync(body: dict):
+    return execute_gql_query_sync(body)
+
+
+def execute_gql_query_sync(body: dict):
+    graphql_request = frappe.parse_json(body)
+    query = graphql_request.query
+    variables = graphql_request.variables
+    operation_name = graphql_request.operationName
+    output = execute(
+        query=query,
+        variables=variables,
+        operation_name=operation_name
+    )
+
+    if len(output.get("errors", [])):
+        frappe.db.rollback()
+        errors = []
+        for err in output.errors:
+            if isinstance(err, GraphQLError):
+                err = err.formatted
+            errors.append(err)
+        output.errors = errors
+    return output
+
+
+import frappe.app
+
+app.mount('/frappe', WSGIMiddleware(frappe.app.application))
+
+
+async def execute_async(query=None, variables=None, operation_name=None):
+    result = await graphql.graphql(
+        # is_awaitable=is_awaitable,
+        schema=get_schema(),
+        source=query,
+        variable_values=variables,
+        operation_name=operation_name,
+        field_resolver=default_field_resolver,
+        middleware=[frappe.get_attr(cmd) for cmd in frappe.get_hooks("graphql_middlewares")],
+        context_value=frappe._dict()
+    )
+    output = frappe._dict()
+    for k in ("data", "errors"):
+        if not getattr(result, k, None):
+            continue
+        output[k] = getattr(result, k)
+    return output
+
+#
+# def is_awaitable(x) -> bool:
+#     return True
+#
+#
+# import inspect
+# import asyncio
+#
+#
+# def isAsync(someFunc):
+#     is_async_gen = inspect.isasyncgenfunction(someFunc)
+#     is_coro_fn = asyncio.iscoroutinefunction(someFunc)
+#     return is_async_gen or is_coro_fn
